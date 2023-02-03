014e82e790300f487328fcfc3faa845e29322b8d
Sondre Malerud <sondre_mal@hotmail.no>
2023-02-03 17:15:23 +0100

    ye

--- src/Oving2/Main.cpp
+++ src/Oving2/Main.cpp
@@ -2,6 +2,7 @@
 # include <iostream>
 # include <list>
 # include <mutex>
+# include <condition_variable>
 # include <thread>
 # include <vector>
 using namespace std;
@@ -9,11 +10,12 @@ using namespace std;
 
 class Workers {
     list<function<void()>> tasks; // Lista med tasks som trådene skal fullføre
+    vector<thread> worker_threads;
+
     int amount_of_threads = 0;
     mutex tasks_mutex;
     condition_variable cv;
-    vector<thread> worker_threads;
-    bool running = true;
+    bool running, wait = true;
 
     public:
         Workers(int amount_of_threads) {
@@ -27,31 +29,50 @@ class Workers {
         for (int i = 0; i < amount_of_threads; i++) {
             worker_threads.emplace_back([this] {
                 while (running) {
-                    //todo
+                    function<void()> task;
+                    {
+                        unique_lock<mutex> lock(tasks_mutex);
+                        while (wait && running){
+                            cv.wait(lock); // Locks thread until it is notified, which happens when post() is used
+                        }
+                        
+                        if (!tasks.empty()) {
+                            task = *tasks.begin(); // Copy task for later use
+                            tasks.pop_front(); // Remove task from list
+                        }
+                    }
+                    if (task) task(); // Run task outside of mutex lock                  
                 }
-    
-
             });
 
+            {
+                unique_lock<mutex> lock(tasks_mutex);
+                wait = false;
+                cv.notify_one();
+            }
         }
-
     }
 
     // Legger til en funksjon i tasks-lista
-    void post(function<void()> task) {
+    void post(function<void()> &task) {
         unique_lock<mutex> lock(tasks_mutex);
-        tasks.emplace_back(task)
+        tasks.emplace_back(task);
         cv.notify_one(); // Vekker én tråd som venter på å utføre en task
     }
 
 
 
-    
+    // Skal stoppe alle trådene når f.eks tasklisten er tom
     void stop() {
-
+        while (running) {
+            if (tasks.empty()) {
+                running = false;
+            }
+        }
     }
 
-    void post_timeout() {
+    // Kjører task argumentet etter et gitt antall milisekund
+    void post_timeout(int miliseconds) {
         
     }
 
@@ -77,23 +98,33 @@ int main() {
     event_loop.start();
 
     worker_threads.post([] {
+        cout << "Task A" << endl;
         // Task A
     });
     worker_threads.post([] {
+        cout << "Task B, might run in parallel with A" << endl;
         // Task B
         // might run in parallel with task A
     });
 
     event_loop.post([] {
+        cout << "Task C, might run in parallell with A and B" << endl;
         // Task C
         // Might run in parallel with task A and B
     });
     event_loop.post([] {
+        cout << "Task D, will run after C, might run in parallel with A and B" << endl;
         // Task D
         // Will run after task C
         // Might run in parallel with task A and B
     });
 
+
     worker_threads.join(); // Calls join() on the worker threads
     event_loop.join(); // Calls join() on the event thread
+
+    worker_threads.stop();
+    event_loop.stop();
+    
+    return 0;
 }
