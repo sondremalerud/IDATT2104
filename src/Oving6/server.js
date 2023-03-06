942df978f31e77b759e04c641b841137754f72e8
Sondre Malerud <sondrmal@stud.ntnu.no>
2023-03-06 15:56:48 +0100

    Work in progress

--- src/Oving6/server.js
+++ src/Oving6/server.js
@@ -1,4 +1,5 @@
 const net = require('net');
+var crypto = require('crypto');
 
 // Simple HTTP server responds with a simple WebSocket client test
 const httpServer = net.createServer((connection) => {
@@ -29,9 +30,38 @@ httpServer.listen(3000, () => {
 // Incomplete WebSocket server
 const wsServer = net.createServer((connection) => {
   console.log('Client connected');
+  var isHandshake = true
 
   connection.on('data', (data) => {
+    var key = ""
+    //TODO: finn key fra klient, og bruk sha + base64endoding (done(?))
+
+    if (isHandshake) {
+
+      const arr = data.toString().split("\n")
+      for (const line in arr) {
+        if (line.includes("Sec-WebSocket-Key")) {
+          key = line.split(" ")[1];
+        }        
+      }
+      var shasum = crypto.createHash('sha1')
+      combined = key+"258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+      shasum.update(combined)
+      encoded = shasum.digest('base64')
+
+      connection.write("HTTP/1.1 101 Switching Protocols\nUpgrade: websocket\nConnection: Upgrade\nSec-WebSocket-Accept: " + encoded)
+      console.log("HTTP/1.1 101 Switching Protocols\nUpgrade: websocket\nConnection: Upgrade\nSec-WebSocket-Accept: " + encoded)
+      isHandshake = false
+    } 
+
     console.log('Data received from client: ', data.toString());
+
+    // TODO: tolk dataen og console log det dekrypterte
+
+
+
+
+
   });
 
   connection.on('end', () => {
@@ -47,6 +77,7 @@ wsServer.on('error', (error) => {
 wsServer.listen(3001, () => {
   console.log('WebSocket server listening on port 3001');
 });
+
 
 
 let bytes = Buffer.from([0x81, 0x83, 0xb4, 0xb5, 0x03, 0x2a, 0xdc, 0xd0, 0x6a]);
